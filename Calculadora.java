cad3167f82bd2b64e63991de1a406d37fcc72b0c
SamuelMaldonado <leumasmal@hotmail.com>
2014-07-21 23:10:48 -0600

    Cuerpo de métodos que buscan un +, -, *, / o un numero y realizan push, pop o las operaciones indicadas

--- Calculadora.java
+++ Calculadora.java
@@ -16,6 +16,9 @@ public class Calculadora {
         data = new StackVector<Integer>();
         File file = new File("Datos.txt");
         String car=null;
+        int operando1 = 0;
+        int operando2 = 0;
+        int resultado = 0;
                 
         
         if(!file.exists()){
@@ -36,28 +39,44 @@ public class Calculadora {
             
         }
         
+        
+        //ciclo que compara cada caracter para saber si hacer push o pop y que operacion realizar
         for (int x=0; x<car.length(); x++){
-	        if (car.charAt(x)=='+'){
-	        	
+	        if (car.charAt(x)=='+'){					//suma los 2 ultimos valores si encuentra un "+"
+	        	operando1 = data.pop();
+	        	operando2 = data.pop();
+	        	resultado = operando1 + operando2;
+	        	data.push(resultado);
 	        }
 
-	        if (car.charAt(x)=='-'){
-	        	
+	        if (car.charAt(x)=='-'){					//resta los 2 ultimos valores si encuentra un "-"
+	        	operando1 = data.pop();
+	        	operando2 = data.pop();
+	        	resultado = operando1 - operando2;
+	        	data.push(resultado);
 	        }
 	       
-	        if (car.charAt(x)=='*'){
-	        	
+	        if (car.charAt(x)=='*'){					//multiplica los 2 ultimos valores si encuentra un "*"
+	        	operando1 = data.pop();
+	        	operando2 = data.pop();
+	        	resultado = operando1 * operando2;
+	        	data.push(resultado);	        	
 	        }
 	        
-	        if (car.charAt(x)=='/'){
-	        	
+	        if (car.charAt(x)=='/'){					//divide los 2 ultimos valores si encuentra un "/"
+	        	operando1 = data.pop();
+	        	operando2 = data.pop();
+	        	resultado = operando1 / operando2;
+	        	data.push(resultado);	        	
 	        }	        
 	        
-	        if (car.charAt(x)=='0' || car.charAt(x)=='1' || car.charAt(x)=='2' || car.charAt(x)=='3' 
+	        if (car.charAt(x)=='0' || car.charAt(x)=='1' || car.charAt(x)=='2' || car.charAt(x)=='3' 			//si encuentra un numero lo guarda en la pila
 	        		 || car.charAt(x)=='4' || car.charAt(x)=='5' || car.charAt(x)=='6' || car.charAt(x)=='7' 
 	        		 || car.charAt(x)=='8' || car.charAt(x)=='9'){
-	        	data.push(car.charAt(x));
+	        	data.push((int)car.charAt(x));
 	        }	        
         }
+        
+        System.out.println(data.pop());					//se imprime el resultado final
     }    
 }
